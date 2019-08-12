b416e8f77fb4188c43cac48f8cba06c4cbdad3ef
Johnathon Wood <jaywood128@gmail.com>
2019-08-12 19:17:33 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: ""
+    }
+
+  }
+  onChange = (event) => {
+    this.setState({
+      text: event.target.value 
+    })
+  }
+  onSubmit = event => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ''
+    })
+  }
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.onSubmit}>
+        <input type="text" onChange={event => this.onChange(event)}></input>
+        <input type="submit" ></input>
+        </form> 
       </div>
     );
   }
--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class Restaurants extends Component {
+  const allRestaurants = this.props.restaurants.map((restaurant) => (<Restaurant restaurant={restaurant} key={restaurant.id}/> ))
   render() {
     return(
       <ul>
