1ff043a6343ae9ed175a9e26305feb82542e538b
Matthew Sherman <msherman0801@gmail.com>
2019-09-10 14:04:36 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+
+  genRestaurants() {
+    return this.props.list.map(restaurant => <li><Restaurant restaurant={restaurant} deleteRestaurant={this.props.deleteRestaurant}/></li>)
+  }
+  
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.genRestaurants()}
       </ul>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
+  restaurantReviews() {
+    return this.props.reviews.filter(review => this.props.restaurant.id === review.restaurantId)
+  }
+
+  genReviews() {
+    return this.restaurantReviews().map(review => <li><Review review={review} deleteReview={this.props.deleteReview}/></li>)
+  }
+
   render() {
     return (
       <ul>
         Reviews
+        {this.genReviews()}
       </ul>
     );
   }
